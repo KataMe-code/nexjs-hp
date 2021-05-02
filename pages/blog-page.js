2907f56518eb8a0974364927afff745a4964315c
KataMe-code <h.k.hiro.2117@gmail.com>
2021-05-02 21:29:56 +0900

    initial comit

---
+++ pages/blog-page.js
@@ -0,0 +1,28 @@
+// Next.js はデフォルトでReactをinportしているのでinport宣言が不要
+import Layout from "../components/Layout";
+import Post from "../components/Post";
+import { getAllPostsdata } from "../lib/posts";
+
+
+const Blog = ({ posts }) => {
+    return (
+        <Layout title="Blog">
+            <ul className="m-10">
+                {posts &&
+                    posts.map((post) => <Post key={post.id} post={post} />)}
+            </ul>
+        </Layout>
+    );
+};
+
+export default Blog;
+
+// 必ずServer Sideで実行される
+// npm run de > 陸枝うsと毎に実行される
+// npm start > ビルド時に実行される
+export async function getStaticProps() {
+    const posts = await getAllPostsdata();
+    return {
+        props: { posts },
+    };
+}
